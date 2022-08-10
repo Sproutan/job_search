bb4378c63c423a840fea3c5f025dc0ea390defff
Sproutan <gab.z.cao@gmail.com[C>
2022-08-09 23:21:18 -0700

    Set up the CSV File and Web App

--- job_search_front_end.py
+++ job_search_front_end.py
@@ -1,10 +1,38 @@
 import job_search
-from flask import Flask
+import csv
+from pandas import read_csv
+from flask import Flask, render_template, request, redirect, url_for
 
-print("Welcome to my Indeed scraper")
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    with open("results.csv") as file:
+        return render_template("index.html", csv=file)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
+
+# print("Welcome to my Indeed scraper")
+
+# job_search.main("associate marketing manager","new york ny")
+# 'JobTitle','Company','Location','PostDate','ExtractDate','Summary','Salary','JobUrl'
 
 
 
+def file_reader():
+    with open("results.csv") as file:
+        reader = csv.reader(file)
 
+        count = 0
 
-job_search.main("associate marketing manager","new york ny")
+        for row in reader:
+            print(row[0])
+
+            if count > 10:
+                break
+
+            count += 1
+
